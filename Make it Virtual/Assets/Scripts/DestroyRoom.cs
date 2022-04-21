f2b986808a474343d55234bc4d9ac8550d548b4c
Jun Kim <junkim100@gmail.com>
2022-04-21 06:53:59 -0400

    Added Scene Change

--- Make it Virtual/Assets/Scripts/DestroyRoom.cs
+++ Make it Virtual/Assets/Scripts/DestroyRoom.cs
@@ -1,33 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GlobalLightToggle : MonoBehaviour
+public class DestroyRoom : MonoBehaviour
 {
-
-    // function EnableLights (enable : boolean) {
-    // for (light in FindObjectsOfType(Light)) light.enabled = enable;
-    // }
 
     public GameObject txtToDisplay;             //display the UI text
     
     private bool PlayerInZone;                  //check if the player is in trigger
 
-    public OVRPassthroughLayer passthorugh;
     public OVRInput.Button button;
     public OVRInput.Controller controller;
 
-    private Light[] lights;
-
     private void Start()
     {
-        lights = FindObjectsOfType(typeof(Light)) as Light[];
-        foreach(Light light in lights) {
-            if(light.tag == "Light") {
-                light.enabled = false;
-            }
-        }
-
         PlayerInZone = false;                   //player not in zone       
         txtToDisplay.SetActive(false);
     }
@@ -36,12 +23,8 @@ public class GlobalLightToggle : MonoBehaviour
     {
         if (OVRInput.GetDown(button, controller))
         {
-            foreach(Light light in lights) {
-                if(light.tag == "Light") {
-                    light.enabled = !light.enabled;
-                }
-            }
             gameObject.GetComponent<Animator>().Play("switch");
+            LoadScene("EscapeRoomDestroy");
         }
     }
 
@@ -62,5 +45,10 @@ public class GlobalLightToggle : MonoBehaviour
             PlayerInZone = false;
             txtToDisplay.SetActive(false);
         }
+    }
+
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
     }
 }
